0e9f68d0e1d94749e292773f55c39a0899a47be2
Ziad-I <68874104+Ziad-I@users.noreply.github.com>
2023-01-01 14:29:18 +0200

    Add: added getter methods for transaction class
    Added getter methods for transaction classes to fix No serializer found for class using Transaction exception thrown when calling checkTransactions

--- Spring/demo/src/main/java/com/example/demo/Client/Transaction.java
+++ Spring/demo/src/main/java/com/example/demo/Client/Transaction.java
@@ -1,5 +1,8 @@
 package com.example.demo.Client;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 import java.util.Vector;
 
 public class Transaction {
@@ -15,4 +18,12 @@ public class Transaction {
     public static Vector<Transaction> refundTransactions =  new Vector<>();
     public static Vector<Transaction> addToWalletTransactions =  new Vector<>();
     public static Vector<Transaction> paymentTransactions =  new Vector<>();
+
+    public String getClientUsername() {
+        return clientUsername;
+    }
+
+    public double getMoney() {
+        return Money;
+    }
 }
